b7c1edaa6ddcdb8ecb5aa1837d4cd802631adb0a
Sanmuell <sanmuell.i@gmail.com>
2021-12-12 17:37:48 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import './App.css';
+import { useEffect , useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const baseUrl="https://localhost:44371/api/Empresa"
+
+const [data, setData]=useState([]);
+
+const peticionGet=async()=>{
+  await axios.get(baseUrl)
+.then(response =>{
+  setData(response.data);
+}).catch(error =>{
+  console.log(error)  
+})
+}
+
+useEffect(()=>{
+  peticionGet();
+},[])
+
+
+return (
+  <div className="App">
+<br/>
+<button className="btn btn-success">Adicionar Empresa</button>
+<br/><br/>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Nome Fantasia</th>
+        <th>UF</th>
+        <th>CNPJ</th>
+      </tr>
+    </thead>
+    <tbody>
+    {data.map(empresa=>(
+      <tr>
+        <td>{empresa.id}</td>
+        <td>{empresa.nomeFantasia}</td>
+        <td>{empresa.uf}</td>
+        <td>{empresa.cnpj}</td>
+        <td>
+          <button className="btn btn-primary"><FontAwesomeIcon icon={faEdit}/></button> {"   "}
+          <button className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt}/></button>
+        </td>
+      </tr>
+      
+
+))}
+    </tbody>
+  </table>
+</div>
+
+ 
   );
 }
 
