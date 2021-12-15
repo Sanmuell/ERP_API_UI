d9488befe75db2567637fa40e5a467a384447d3f
Sanmuell <sanmuell.i@gmail.com>
2021-12-14 21:05:37 -0300

    Put Empresa

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ function App() {
   const baseUrl = "https://localhost:44371/api/Empresa";
   const [data, setData] = useState([]);
   const [modalInserir, setModalInserir] = useState(false);
+  const [modalEditar, setModalEditar] = useState(false);
   const [empresaSelecionada, setEmpresaSelecionada] = useState({
+    id: '',
     nomeFantasia: '',
     uf: '',
     cnpj: ''
@@ -28,6 +30,9 @@ function App() {
 
   const abrirFecharModalInserir = () => {
     setModalInserir(!modalInserir);
+  }
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
   }
 
 
@@ -58,14 +63,46 @@ function App() {
         } else {
 
           console.log('Error', error.message);
-
         }
-      });
-
-
-
-
+      })
   }
+
+  const peticionPut = async () => {
+    await axios.put(baseUrl + "/" + empresaSelecionada.id, empresaSelecionada)
+      .then(response => {
+        var resp = response.data;
+        var dataAuxiliar = data;
+        dataAuxiliar.map(empresa => {
+          if (empresa.id === empresaSelecionada.id) {
+            empresa.nomeFantasia = resp.nomeFantasia;
+            empresa.uf = resp.uf;
+            empresa.cnpj = resp.cnpj
+          }
+        })
+        setData(data.concat(response.data));
+        abrirFecharModalEditar();
+      }).catch(error => {
+        if (error.response) {
+
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+
+          console.log(error.request);
+        } else {
+
+          console.log('Error', error.message);
+        }
+      })
+  }
+
+  const selecionarEmpresa = (empresa, caso) => {
+    setEmpresaSelecionada(empresa);
+    (caso === "Editar") &&
+      abrirFecharModalEditar();
+  }
+
   useEffect(() => {
     peticionGet();
   }, [])
@@ -93,7 +130,7 @@ function App() {
               <td>{empresa.uf}</td>
               <td>{empresa.cnpj}</td>
               <td>
-                <button className="btn btn-primary"><FontAwesomeIcon icon={faEdit} /></button> {"   "}
+                <button className="btn btn-primary" onClick={() => selecionarEmpresa(empresa, "Editar")}><FontAwesomeIcon icon={faEdit} /></button> {"   "}
                 <button className="btn btn-danger"><FontAwesomeIcon icon={faTrashAlt} /></button>
               </td>
             </tr>
@@ -123,6 +160,38 @@ function App() {
         <ModalFooter>
           <button className="btn btn-primary" onClick={() => peticionPost()} >Inserir</button>{"   "}
           <button className="btn btn-danger" onClick={() => abrirFecharModalInserir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+
+
+
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>Inserir Emrpesa</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID: </label>
+            <br />
+            <input type="text" className="form-control" name="id" readOnly value={empresaSelecionada && empresaSelecionada.id}></input>
+            <br />
+            <label>Nome Fantasia: </label>
+            <br />
+            <input type="text" className="form-control" name="nomeFantasia" onChange={handleChange} value={empresaSelecionada && empresaSelecionada.nomeFantasia}  ></input>
+            <br />
+            <label>UF: </label>
+            <br />
+            <input type="text" className="form-control" name="uf" onChange={handleChange} value={empresaSelecionada && empresaSelecionada.uf}></input>
+            <br />
+            <label>CNPJ: </label>
+            <br />
+            <input type="text" className="form-control" name="cnpj" onChange={handleChange} value={empresaSelecionada && empresaSelecionada.cnpj}></input>
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={() => peticionPut()} >Inserir</button>{"   "}
+          <button className="btn btn-danger" onClick={() => abrirFecharModalEditar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
 
